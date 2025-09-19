5e8cd4ba3b2fdd56090cda95e9b298d7ac55458c
Srinivas <srinidevopsdev@gmail.com>
2025-09-19 19:25:14 +0530

    Added one simple conversation of 2

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -5,7 +5,7 @@
 person1=jaswitha
 person2=shaurya
 
-echo "$person1::How are you?"
-echo "$person2::I am fine, how are you?"
-echo "$person1::I am fine, what you doing?"
-echo "$person2::I am learning DevOps"
+echo "$person1::How are you $person2?"
+echo "$person2::I am fine $person1, how are you?"
+echo "$person1::I am fine $person2, what you doing?"
+echo "$person2::I am learning DevOps $person1"
