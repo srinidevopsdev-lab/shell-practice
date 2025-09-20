8a28bfab78a347fd733892bc0ea86259b4f052c0
Srinivas <srinidevopsdev@gmail.com>
2025-09-20 21:18:05 +0530

    Read parameter

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,5 +7,7 @@ echo "All variables passing in this script:$@"
 echo "All variables passing in this script:$*"
 
 echo "Script name is:$0"
-
+echo "Current directory:$PWD"
+echo "Who is running script:$USER"
+echo "Home directory:$HOME"
 echo "Process ID is:$$"
