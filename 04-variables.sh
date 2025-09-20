02996532c7186a995ec4cd7761852de9983c5785
Srinivas <srinidevopsdev@gmail.com>
2025-09-20 20:59:58 +0530

    Read parameter

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Below variables we are passing in this script
+echo "================================================"
+
+echo "All variables passing in this script:$@"
+echo "All variables passing in this script:$@"
