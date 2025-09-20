5b51cda8164172f7a826b6cd9e193029a4afaf32
Srinivas <srinidevopsdev@gmail.com>
2025-09-20 10:50:05 +0530

    Read parameter

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,7 +4,7 @@ echo "============================================="
 
 echo "Please enter your Pin: "
 
-read  PIN_NUMBER # It will read the pin
+read  -s PIN_NUMBER # It will read the pin
 
 echo "Entered pin is $PIN_NUMBER"
 
