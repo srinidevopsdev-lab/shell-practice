207d5a36f2ed4e4418b77b48cae9ce6d8ded93d5
Srinivas <srinidevopsdev@gmail.com>
2025-09-20 20:54:47 +0530

    Read parameter

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-DATE=$(date +%s)
+# DATE=$(date +%s)
 
-echo "Today date is: $DATE"
+START_DATE=$(date +%s)
+
+sleep 10
+
+END_DATE=$(date +%s)
+
+TOTAL_TIME=$(($START_DATE-$END_DATE))
+
+echo "Today date is: $TOTAL_TIME"
