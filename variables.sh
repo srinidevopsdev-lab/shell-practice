d3ba00569e8279cf4826b4e6120d2e6e2476331c
Srinivas <srinidevopsdev@gmail.com>
2025-09-20 08:51:31 +0530

    Added one simple conversation

---
+++ variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Just for information 
+
+person1=$1
+person2=$2
+echo "======================Welcome==============================="
+
+echo "$person1::How are you $person2?"
+echo "$person2::I am fine $person1, how are you?"
+echo "$person1::I am fine $person2, what you doing?"
+echo "$person2::I am learning DevOps $person1...!"
+
+echo "=================ThankYou==================================="
