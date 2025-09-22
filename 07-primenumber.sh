38de935a73ba0f983fba19c00ac201a5b63f4b77
Srinivas <srinidevopsdev@gmail.com>
2025-09-22 20:55:16 +0530

    Read parameter

--- 07-primenumber.sh
+++ 07-primenumber.sh
@@ -11,3 +11,19 @@ elif [ "$num" -eq 2 ]; then
     echo "$num is a prime number."
     exit 0
 fi
+
+is_prime=true # Flag to track primality
+
+# Iterate from 2 up to half of the number
+for (( i=2; i*i<=$num; i++ )); do
+    if (( num % i == 0 )); then
+        is_prime=false
+        break
+    fi
+done
+
+if [ "$is_prime" = true ]; then
+    echo "$num is a prime number."
+else
+    echo "$num is not a prime number."
+fi
