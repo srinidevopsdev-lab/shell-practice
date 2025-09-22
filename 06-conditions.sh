79866dd7a65dec45f344cef25068627eb154ab03
Srinivas <srinidevopsdev@gmail.com>
2025-09-22 20:10:21 +0530

    Read parameter

---
+++ 06-conditions.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+# Even and Odd number finding
+
+echo "Please enter the number::"
+
+read NUMBER
+
+if [ $((15%2 -eq 0)) ]; then
+    echo "The given number $NUMBER is Even number"
+else 
+    echo "The given number $NUMBER is Odd number"
+fi
