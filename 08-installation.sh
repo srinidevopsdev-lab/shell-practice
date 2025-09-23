53dea47866f1c9ac82ad08fdde5c9f789bf2a0cd
Srinivas <srinidevopsdev@gmail.com>
2025-09-24 04:24:41 +0530

    Read parameter

---
+++ 08-installation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+
+if [ $USERID -ne 0 ]; then
+    echo "Error::Please run this script as root user"
+    exit 1 # failure is othere than 0 Means it will stop here dont run furthur
+fi
+
+VALIDATE () {
+    if [ $1 -ne 0 ]; then
+        echo "Error: $2 installation is failure"
+        exit 1 # 1 for failure
+    else
+        echo "$2 installation is successful"
+}
+    
+dnf install mysql -y
+VALIDATE $? "Mysql"
+
+dnf install mysql -y
+VALIDATE $? "Nginx"
+
+dnf install mysql -y
+VALIDATE $? "Mongodb"
+    
+
