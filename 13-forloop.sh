d812aa56215beae4e58ce513822583ce8f1b249b
Srinivas <srinidevopsdev@gmail.com>
2025-09-27 14:30:20 +0530

    number check

--- 13-forloop.sh
+++ 13-forloop.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Iterate with a step
-for (( j=0; j<10; j+=2 )); do
-  echo "Even number: $j"
+for i in 1 2 3 4 5 6 7 8 9 10; do
+  if [ $i -eq 5 ]; then
+    echo "Breaking the loop at $i"
+    break
+  fi
+  if [ $i -eq 3 ]; then
+    echo "Skipping iteration $i"
+    continue
+  fi
+  echo "Iteration number: $i"
 done
