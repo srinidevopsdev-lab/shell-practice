3c4faa7abd98afe6f55e7bb8abadd4073472523e
Srinivas <srinidevopsdev@gmail.com>
2025-09-27 20:15:27 +0530

    number check

--- 13-forloop.sh
+++ 13-forloop.sh
@@ -1,13 +1,5 @@
-#!/bin/bash
-
-for i in 1..10; do
-  if [ $i -eq 5 ]; then
-    echo "Breaking the loop at $i"
-    break
-  fi
-  if [ $i -eq 3 ]; then
-    echo "Skipping iteration $i"
-    continue
-  fi
-  echo "Iteration number: $i"
-done
+    #!/bin/bash
+    
+    echo "What is your name?"
+    read name
+    echo "Hello, $name!"
