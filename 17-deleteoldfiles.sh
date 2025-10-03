ee0e2d109b9ad535c55cda77f04ceb2c2d107cd5
Srinivas <srinidevopsdev@gmail.com>
2025-10-04 04:13:51 +0530

    delete old files

--- 17-deleteoldfiles.sh
+++ 17-deleteoldfiles.sh
@@ -6,19 +6,21 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FOLDER="/var/log/shell-practice"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+
+if [ $user_id -ne 0 ]; then
+    echo -e "error:: Run this script as root user"
+    exit 1
+fi
 
 mkdir -p $LOGS_FOLDER
 echo "script started executed at: $(date)" | tee -a $LOG_FILE
 
 source_dir=/home/ec2-user
 
-if [ $user_id -ne 0 ]; then
-    echo -e "error:: Run this script as root user"
-    exit 1
-fi
+
 
 if [ ! -d $source_dir ]; then
     echo -e "error:: $source_dir is doesn't exist"
